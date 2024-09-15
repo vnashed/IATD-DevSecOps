b1ffcfbd06e247e6356edf86132595ca6dd83f65
Veronica <veronica.nashed@gmail.com>
2024-09-15 15:35:36 +1000

    DAST

--- models/user_model.py
+++ models/user_model.py
@@ -45,6 +45,7 @@ class User(db.Model):
     def decode_auth_token(auth_token):
         try:
             if vuln:
+                #Week 3- This is a security issue detected by SonarCube (verify=False)
                 payload = jwt.decode(auth_token, vuln_app.app.config.get('SECRET_KEY'), algorithms=["HS256","none"], options={"verify_signature": False}, verify=False )
             else:
                 payload = jwt.decode(auth_token, vuln_app.app.config.get('SECRET_KEY'), algorithms=["HS256"])
